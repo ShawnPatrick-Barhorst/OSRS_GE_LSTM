9edef301eac57a496cda986bb80a683fa8d7a3de
ShawnPatrick-Barhorst <shawnpatrick.barhorst@gmail.com>
2024-08-23 23:59:58 -0400

    updated

--- osrs_torch.py
+++ osrs_torch.py
@@ -7,7 +7,35 @@ import copy
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
+#scaler = MinMaxScaler(feature_range=(0,1))
 scaler = StandardScaler()
+
+"""#Data"""
+
+dataset = pd.read_csv('https://raw.githubusercontent.com/ShawnPatrick-Barhorst/OSRS_GE_LSTM/main/OSRS_GE_price_dataset.csv')
+dataset
+
+train = dataset['Anchovy pizza'][:5500].ewm(com=10).mean()
+train = train.pct_change()
+train = train.fillna(method = 'bfill')
+test = dataset['Pineapple pizza'][:5500].ewm(com=10).mean()
+test = test.pct_change()
+test.dropna()
+
+train = np.array(train)
+train = np.reshape(train, (-1, 1))
+scaled_data = scaler.fit_transform(train)
+
+window_size = 30
+x_train, y_train = [], []
+for i in range(window_size,5500):
+    x_train.append(scaled_data[i-window_size:i,0])
+    y_train.append(scaled_data[i,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+"""
+#Pytorch LSTM
+"""
 
 import torch
 import torch.nn as nn
@@ -23,7 +51,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class data(Dataset):
-  def __init__(self, exclusion_set, split_ratio, train):
+  def __init__(self, exclusion_set, split_ratio, train, window_length = 30):
     dataset = pd.read_csv('https://raw.githubusercontent.com/ShawnPatrick-Barhorst/OSRS_GE_LSTM/main/OSRS_GE_price_dataset.csv')
     dataset = dataset.astype('float64')
 
@@ -39,13 +67,17 @@ class data(Dataset):
 
     print(dataset.shape)
 
-    item_list = [item for item in dataset.columns if item not in exclusion_set][:1000]
+
+
+
+    item_list = [item for item in dataset.columns if item not in exclusion_set][:10]
 
     x_train, y_train = [], []
     for item in item_list:
       train = dataset[item][:5500].ewm(com=10).mean()
       train = train.pct_change()
       train = train.fillna(method = 'bfill')
+
 
       train = np.array(train)
       train = np.reshape(train, (-1, 1))
@@ -55,17 +87,23 @@ class data(Dataset):
 
       scaled_data = scaler.fit_transform(train)
 
-      for i in range(60,5500):
-        x_train.append(scaled_data[i-60:i,0])
+      noise = np.random.normal(0, 0.01, size=scaled_data.shape)
+      scaled_data = scaled_data + noise
+
+
+      for i in range(window_length,5500):
+        x_train.append(scaled_data[i-window_length:i,0])
         y_train.append(scaled_data[i,0])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
 
-    x_train = x_train.reshape(-1, 60, 1)
+    x_train = x_train.reshape(-1, window_length, 1)
     y_train = y_train.reshape(-1, 1)
 
     self.x = torch.from_numpy(x_train).float()
     self.y = torch.from_numpy(y_train).float()
+
+
 
     print(x_train.shape)
     print(y_train.shape)
@@ -85,10 +123,12 @@ validation_data = data(exclusion_set, train=False, split_ratio=0.8)
 
 validation_data.get_validation_list()
 
-batch_size = 256
+batch_size = 16
 
 trainloader = torch.utils.data.DataLoader(train_data, batch_size, shuffle=True)
 validloader = torch.utils.data.DataLoader(validation_data, batch_size, shuffle=True)
+
+"""data is in format (window sample, time steps, features)"""
 
 class LSTM_block(nn.Module):
 
@@ -98,7 +138,7 @@ class LSTM_block(nn.Module):
     self.lstm_layer = nn.LSTM(input_dim, hidden_dim, batch_first=True)
     self.batch_norm = nn.BatchNorm1d(hidden_dim)
     self.leaky_relu = nn.LeakyReLU()
-    self.dropout = nn.Dropout(0.2)
+    self.dropout = nn.Dropout(0.3)
 
   def forward(self, x):
     x, _ = self.lstm_layer(x)
@@ -118,7 +158,7 @@ class Dense_block(nn.Module):
     self.linear_layer = nn.Linear(input_dim, hidden_dim)
     self.batch_norm = nn.BatchNorm1d(hidden_dim)
     self.leaky_relu = nn.LeakyReLU()
-    self.dropout = nn.Dropout(0.2)
+    self.dropout = nn.Dropout(0.3)
 
   def forward(self, x):
     x = self.linear_layer(x)
@@ -130,7 +170,7 @@ class Dense_block(nn.Module):
     return x
 
 
-class model(nn.Module):
+class LSTM_model(nn.Module):
 
   def __init__(self, input_dim, hidden_dim):
     super(LSTM_model, self).__init__()
@@ -147,8 +187,8 @@ class model(nn.Module):
 
   def forward(self, x):
     x = self.lstm_1(x)
-    x = self.lstm_2(x)
-    x = self.lstm_3(x)
+    #x = self.lstm_2(x)
+    #x = self.lstm_3(x)
 
     x = self.dense_1(x)
     x = self.dense_2(x)
@@ -172,12 +212,12 @@ class LogCosH(nn.Module):
 epochs = 20
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.01)
 criterion = LogCosH()
 
 model.to(device)
 
-def train_model(model, trainloader, validloader, epochs, device, threshold=0.001, stop_patience=3):
+def train_model(model, trainloader, validloader, epochs, device, threshold=0.01, stop_patience=5):
   best_val_loss = float('inf')
 
 
@@ -217,8 +257,8 @@ def train_model(model, trainloader, validloader, epochs, device, threshold=0.001
             ))
 
       #Early Stopping
-      if val_loss < best_val_loss - threshold:
-        best_val_loss = val_loss
+      if avg_loss < best_val_loss + threshold:
+        best_val_loss = avg_loss
         patience = 0
         best_model_weights = copy.deepcopy(model.state_dict())
       else:
@@ -230,3 +270,7 @@ def train_model(model, trainloader, validloader, epochs, device, threshold=0.001
 train_model(model, trainloader, validloader, epochs, device)
 
 torch.save(model.state_dict(), 'LSTM_model.pt')
+
+new_model = LSTM_model(1, 32)
+new_model.load_state_dict(torch.load('LSTM_model.pt'))
+new_model.to(device)
